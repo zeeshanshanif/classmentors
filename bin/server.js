0a5b4c68379a3bda5987a369cb7894a7d18a4c1c
Damien Lebrun <dinoboff@hotmail.com>
2016-03-15 17:07:55 +0000

    Add dev server a proxy to the 3rd party profile

--- bin/server.js
+++ bin/server.js
@@ -6,6 +6,7 @@ const http2 = require('http2');
 const morgan = require('morgan');
 const path = require('path');
 const serveStatic = require('serve-static');
+const https = require('https');
 
 const www = process.argv.slice(2).pop() || path.join(__dirname, '../src');
 const cert = fs.readFileSync(path.join(__dirname, '/localhost.cert'));
@@ -14,18 +15,50 @@ const port = process.env.npm_package_config_port || 8081;
 
 const logger = morgan('dev');
 const serve = serveStatic(www, {index: ['index.html']});
+const proxy = (req, resp, next) => {
+  const url = req.url.replace(/\/+/, '/');
+  const meth = req.method;
 
-const server = http2.createServer({key, cert}, (req, resp) => {
-  const done = finalhandler(req, resp);
+  if (meth !== 'GET' || !url.startsWith('/proxy/')) {
+    return next();
+  }
 
-  logger(req, resp, err => {
-    if (err) {
-      done(err);
-    }
+  const remote = `https://${url.slice(7)}`;
 
-    serve(req, resp, done);
+  https.get(remote, (proxyResp) => {
+    // console.log("%s %j", proxyResp.statusCode, proxyResp.headers);
+    const allowedHeaders = new Set(['date', 'content-type', 'content-length', 'vary']);
+
+    resp.writeHead(proxyResp.statusCode, Object.keys(proxyResp.headers).reduce((headers, k) => {
+      if (allowedHeaders.has(k)) {
+        headers[k] = proxyResp.headers[k];
+      }
+
+      return headers;
+    }));
+
+    proxyResp.on('data', chunk => {
+      resp.write(chunk, 'bimary');
+    });
+
+    proxyResp.on('end', () => {
+      resp.end();
+    });
   });
-});
+};
+const use = (req, resp, middlewares) => {
+  middlewares.reverse().reduce((next, handler) => {
+    return err => {
+      if (err) {
+        next(err);
+      }
+
+      handler(req, resp, next);
+    };
+  }, finalhandler(req, resp))();
+};
+
+const server = http2.createServer({key, cert}, (req, resp) => use(req, resp, [logger, proxy, serve]));
 
 server.listen(port, () => console.log(
   `Dev server listening on https://localhost:${port} (don't forget to use "HTTPS")`
