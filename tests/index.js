30faecadaa75da24716a88b09a38c3a0d08977b4
zeeshanshanif <zeeshanhanifpc@gmail.com>
2016-05-17 20:26:08 +0500

    Added simple javascript test case

--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,14 @@
-console.log('TODO');
+// console.log('TODO');
+
+(function() {
+    'use strict';
+
+    var assert = require('assert');
+
+    describe('dummy-test-case', function() {
+        it('It should give true', function() {
+            assert.equal( 2 == 2, true);
+        });
+    });
+
+})();
